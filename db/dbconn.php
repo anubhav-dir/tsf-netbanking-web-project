2f498734f7bbee3a6e61114a1f674d52531b8de5
anubhav-dir <84168450+anubhav-dir@users.noreply.github.com>
2021-07-16 10:03:16 +0530

    db conn

--- db/dbconn.php
+++ db/dbconn.php
@@ -1,26 +1,24 @@
 <?php
 
 // localhost
-// $dbhost = "localhost";
-// $dbname = "tsf-netbanking";
-// $dbusername = "root";
-// $dbpassword = "";
-// $charset = "utf8mb4";
+$dbhost = "localhost";
+$dbname = "tsf-netbanking";
+$dbusername = "root";
+$dbpassword = "";
 
 // remotehost
 // $dbhost = "remotemysql.com";
 // $dbname = "u54j1egpnP";
 // $dbusername = "u54j1egpnP";
 // $dbpassword = "mTN6TSVoPS";
-// $charset = "utf8mb4";
 
 // AwardSpace
-$dbhost = "fdb34.awardspace.net";
-$dbusername = "3894080_netbanking";
-$dbpassword = "[tFh3Ztd3+?%P+;/";
-$dbname = "3894080_netbanking";
-$charset = "utf8mb4";
+// $dbhost = "fdb34.awardspace.net";
+// $dbusername = "3894080_netbanking";
+// $dbpassword = "[tFh3Ztd3+?%P+;/";
+// $dbname = "3894080_netbanking";
 
+$charset = "utf8mb4";
 $dns = "mysql:host=$dbhost; dbname=$dbname; charset=$charset";
 
 try {
